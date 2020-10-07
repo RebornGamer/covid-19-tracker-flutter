240d072ab1f7339bfcefc2efcf58dd33514a4a5d
RebornGamer <schaudhary240@gmail.com>
2020-10-07 13:18:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/reborn/covid19tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.reborn.covid19tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/datasource.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+Color primaryBlack = Color(0xff202c3b);
+
+class DataSource {
+  static String quote =
+      'Nothing in Life is to be feared,it is only to be understood. Now is the time to understand more, so that we may fear less';
+
+  static List questionAnswers = [
+    {
+      "question": "What is a CoronaVirus?",
+      "answer":
+          "Coronaviruses is a large family of viruses which may cause illness in animals or humans ."
+    },
+    {
+      "question": "What is COVID-19?",
+      "answer":
+          "Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus."
+    },
+    {
+      "question": "What are the symptoms of COVID-19?",
+      "answer":
+          "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, or sore throat. These symptoms are usually mild and begin gradually."
+    },
+    {
+      "question": "How does COVID-19 Spreads?",
+      "answer":
+          "The virus is primarily spread through contact and respiratory droplets. Under some circumstances airborne transmission may occur (such as when aerosol generating procedures are conducted in health care settings or potentially, in indoor crowded poorly ventilated settings elsewhere)."
+    },
+    {
+      "question":
+          "Can the virus that causes COVID-19 be transmitted through air?",
+      "answer":
+          "Research shows that the virus can live in the air for up to 3 hours. It can get into your lungs if someone who has it breathes out and you breathe that air in. Experts are divided on how often the virus spreads through the airborne route and how much it contributes to the pandemic."
+    },
+    {
+      "question": "Can COVID-19 be caught from a person who has no symptoms?",
+      "answer":
+          "Yes, infected people can transmit the virus both when they have symptoms and when they don’t have symptoms. "
+    },
+    {
+      "question": "How likely am I to catch COVID-19?",
+      "answer":
+          "The risk depends on where you are - and more specifically, whether there is a COVID-19 outbreak unfolding there."
+    },
+    {
+      "question": "Who is at risk of developing severe illness?",
+      "answer":
+          "Most people (about 80%) recover from the disease without needing special treatment, and for the majority – especially for children and young adults – illness due to COVID-19 is generally minor."
+    },
+    {
+      "question": "Should I wear a mask to protect myself?",
+      "answer":
+          "Wearing a mask without the proper training on how to fit it to the face does not provide you with meaningful protection from a respiratory virus, and may increase the risk of transmission if not removed and disposed of correctly. There is no evidence to support wearing a mask in people who are not sick. Public Health recommends everyday prevention actions to reduce the spread of the virus, such as hand washing, avoiding touching your eyes or nose and covering your cough or sneeze with a tissue."
+    },
+    {
+      "question":
+          "Are AntiBiotics effective in preventing or treating the COVID-19?",
+      "answer":
+          "No. AntiBiotics do not work against viruses, they only work on bacterial diseases."
+    }
+  ];
+}
